4cc3fe32ff38b81b4cf2857e93d04e63b5c4b576
Dev  Mode <dev-mode@MacBook-Pro.local>
2024-01-09 22:53:50 +0600

    query writtings

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ContactController extends Controller
 {
     function page(Request $request){
-
+        $seo = DB::table('seoproperties')->where('pageName','=','Home')->first();
+        return view('pages.contact',['seo'=>$seo]);
     }
     function contactRequest(Request $request){
-
+        return DB::table('contacts')->insert($request->input());
     }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,19 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     function page(Request $request){
-
+        $seo = DB::table('seoproperties')->where('pageName','=','Home')->first();
+        return view('pages.home',['seo'=>$seo]);
     }
     function heroData(Request $request){
-
+        return DB::table('heroproperties')->first();
     }
     function aboutData(Request $request){
-
+        return DB::table('abouts')->first();
     }
     function socialData(Request $request){
-
+        return DB::table('socials')->first();
     }
 }
