1297469e41b0a3e481b367959e27e9494ed22d9f
Dev  Mode <dev-mode@MacBook-Pro.local>
2024-01-09 13:52:23 +0600

    controller and methods define

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    function page(Request $request){
+
+    }
+    function heroData(Request $request){
+
+    }
+    function aboutData(Request $request){
+
+    }
+    function socialData(Request $request){
+
+    }
+}
---
+++ app/Http/Controllers/ResumeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ResumeController extends Controller
+{
+   function page(Request $request){
+
+   }
+    function resumeLink(Request $request){
+
+    }
+    function experienceData(Request $request){
+
+    }
+    function educationData(Request $request){
+
+    }
+    function skillData(Request $request){
+
+    }
+    function languageData(Request $request){
+
+    }
+}
