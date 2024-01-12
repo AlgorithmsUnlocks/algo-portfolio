da382253b900a9fbf2537d8f1314452bb51fab17
Dev  Mode <dev-mode@MacBook-Pro.local>
2024-01-12 14:01:31 +0600

    final-build

--- routes/web.php
+++ routes/web.php
@@ -30,16 +30,16 @@ Route::get('/resume',[ResumeController::class,'page']);
 * Ajax call Routes for algo-portfolio
 */
 
-Route::get('/heroData',[HomeController::class,'heroData']); // done
-Route::get('/aboutData',[HomeController::class,'aboutData']); // done
-Route::get('/socialData',[HomeController::class,'socialData']); // done
+Route::get('/heroData',[HomeController::class,'heroData']);
+Route::get('/aboutData',[HomeController::class,'aboutData']);
+Route::get('/socialData',[HomeController::class,'socialData']);
 
-Route::get('/projectData',[ProjectController::class,'projectData']); // done
+Route::get('/projectData',[ProjectController::class,'projectData']);
 
-Route::get('/resumeLink',[ResumeController::class,'resumeLink']); // done
-Route::get('/experienceData',[ResumeController::class,'experienceData']); // done
-Route::get('/educationData',[ResumeController::class,'educationData']); // done
+Route::get('/resumeLink',[ResumeController::class,'resumeLink']);
+Route::get('/experienceData',[ResumeController::class,'experienceData']);
+Route::get('/educationData',[ResumeController::class,'educationData']);
 Route::get('/skillData',[ResumeController::class,'skillData']);
-Route::get('/languageData',[ResumeController::class,'languageData']); // done
+Route::get('/languageData',[ResumeController::class,'languageData']);
 
-Route::post('/contactRequest',[ContactController::class,'contactRequest']); //done
+Route::post('/contactRequest',[ContactController::class,'contactRequest']);
